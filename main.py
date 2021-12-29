685ba23a3a3d404e8d1d67feeecc96154b45d23a
msprat <ms.pratheek1@gmail.com>
2021-12-29 22:38:38 +0530

    post request created

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
 
 
 app = FastAPI()
@@ -6,4 +8,14 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World!!"}
+
+@app.get("/post")
+def get_post():
+    return {"message": "posts are here"}
+
+@app.post("/createposts")
+def create_posts(payload: dict=Body(...)):
+    print(payload)
+    print(payload["desc"])
+    return {"message":f"titlle {payload['tittle']}"}
