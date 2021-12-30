3a40fee6e972b1229cd6735bd01c1932d7b1e095
msprat <ms.pratheek1@gmail.com>
2021-12-30 20:54:10 +0530

    table created with sqlalchemy

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy.sql.expression import text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
@@ -8,4 +10,5 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, default=True)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'))
