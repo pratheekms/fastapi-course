72536cf8e561f7da031c25e3da6d6e10f54c28c0
msprat <ms.pratheek1@gmail.com>
2021-12-30 12:47:52 +0530

    post completed

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from typing import Optional
 from fastapi import FastAPI
-from fastapi.params import Body
-
+# from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1",
+             "content": "content of post 1", "id": 1},
+            {"title": "title of post 2",
+             "content": "content of post 2", "id": 2}
+            ]
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!!"}
 
-@app.get("/post")
-def get_post():
-    return {"message": "posts are here"}
 
-@app.post("/createposts")
-def create_posts(payload: dict=Body(...)):
-    print(payload)
-    print(payload["desc"])
-    return {"message":f"titlle {payload['tittle']}"}
+@app.get("/posts")
+def get_post():
+    return {"message": my_posts}
+
+
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 100000000)
+    my_posts.append(post_dict)
+    return {"message": post_dict}
