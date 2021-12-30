61d3e39dec827bdf10293e07b072a2d6ca61ab3a
msprat <ms.pratheek1@gmail.com>
2021-12-30 14:52:54 +0530

    post completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from fastapi.exceptions import HTTPException
 # from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+
 
 app = FastAPI()
 
@@ -21,19 +23,39 @@ my_posts = [{"title": "title of post 1",
             ]
 
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World!!"}
 
 
 @app.get("/posts")
-def get_post():
+def get_posts():
     return {"message": my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict["id"] = randrange(0, 100000000)
     my_posts.append(post_dict)
     return {"message": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    print(id)
+    post = find_post(id)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='not found!!!')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": "post not found"}
+    return {"post details": post}
+
+
