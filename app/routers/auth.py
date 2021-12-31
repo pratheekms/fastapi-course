6a2f967a453007f5d119e468356185df1e1107fc
msprat <ms.pratheek1@gmail.com>
2021-12-31 16:51:22 +0530

    bearer token implementation completed

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
-
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
-from .. import models, schemas, utils
+from .. import models, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
+          db: Session = Depends(get_db)):
     user = db.query(models.User).filter(
-        models.User.email == user_credentials.email).first()
+        models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -21,4 +22,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
             status_code=status.HTTP_404_NOT_FOUND,
             detail='Invalid creadentials!')
 
-    return {"token": "new token"}
+    token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": token, "token_type": "bearer"}
