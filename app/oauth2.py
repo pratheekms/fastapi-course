811bfc7642cb4c55aee8af88d106ebd68649c341
msprat <ms.pratheek1@gmail.com>
2021-12-31 22:37:06 +0530

    bearer token auth bug fix

--- app/oauth2.py
+++ app/oauth2.py
@@ -44,7 +44,7 @@ def get_current_user(token: str = Depends(oauth2_schema), db: Session = Depends(
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail="Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
-    token = verify_access_token(token, credentials_exception)
-    user = db.query(models.User).filter(models.User.id == token.id).first()
+    token_data = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token_data.id).first()
 
     return user
