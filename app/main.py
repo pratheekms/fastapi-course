7bff321512763e76edede4671ec2c90ac71441c1
msprat <ms.pratheek1@gmail.com>
2021-12-31 13:19:00 +0530

    added root end point

--- app/main.py
+++ app/main.py
@@ -13,5 +13,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
 app.include_router(post.router)
 app.include_router(user.router)
