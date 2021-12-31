8add18d04359578e938c64481c78ee673b074707
msprat <ms.pratheek1@gmail.com>
2021-12-31 22:29:17 +0530

    bearer token auth completed for posts

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -9,7 +9,7 @@ from .. import models, utils, oauth2
 router = APIRouter(tags=["Authentication"])
 
 
-@router.post('/login',response_model=schemas.Token)
+@router.post('/login', response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(),
           db: Session = Depends(get_db)):
     user = db.query(models.User).filter(
